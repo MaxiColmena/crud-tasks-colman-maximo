1edfbbfdf008ee04b02d86b48735d0fd5cf4a9f0
Colman Maximo <colmanmaximo2003@gmail.com>
2025-08-08 14:33:20 -0300

    Se realizó la configuración del servidor para poder inicializarlo

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import { routerTask } from "./src/routes/task.routes.js";
+import { routerUser } from "./src/routes/user.routes.js";
+import startDB from "./config/db.js";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.json());
+app.use("/api", CharacterRoutes);
+
+startDB().then(()=>{app.listen(PORT, ()=>{
+    console.log("Escuchando en el puerto:", PORT);
+})});
