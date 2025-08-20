2c74c9f046dcb37f142c3cdb346764af6fa6e24c
Colman Maximo <colmanmaximo2003@gmail.com>
2025-08-20 12:35:05 -0300

    Se realizaron las rutas y los modelos de cada tabla

--- src/models/addres.model.js
+++ src/models/addres.model.js
@@ -1,24 +1,25 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/database.js";
+import { UserModel } from "./user.model.js";
 
-export const Address = sequelize.define("addresses", {
-    address_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+export const AddressModel = sequelize.define(
+    "Address",
+    {
+        address_id: {type: DataTypes.INTEGER, primaryKey: true, allowNull: false},
+        street: { type: DataTypes.STRING(100), allowNull: false},
+        street_number: { type: DataTypes.INTEGER, allowNull: false},
+        neighborhood: { type: DataTypes.STRING(100), allowNull: false}
     },
-    street: {
-        type: DataTypes.STRING(200),
-        allowNull: false
-    },
-    street_number: {
-        type: DataTypes.STRING(20),
-        allowNull: false
-    },
-    neighborhood: {
-        type: DataTypes.STRING(100),
-        allowNull: false
+    {
+        timestamps: false,
     }
-}, {
-    timestamps: false
-});
+)
+UserModel.belongsTo(AddressModel, {
+    foreignKey: "address_id",
+    as: "address"
+})
+
+AddressModel.hasOne(UserModel, {
+    foreignKey: "address_id",
+    as: "usuario1"
+})
